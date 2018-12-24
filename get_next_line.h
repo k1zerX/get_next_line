7d35d1c7db8e476a5ce2e5b268ef13aeddd5a011
Kaleb Batz <kbatz@e1r3p4.21-school.ru>
2018-12-24 18:14:48 +0300

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/25 23:20:28 by kbatz             #+#    #+#             */
+/*   Updated: 2018/12/24 17:50:34 by kbatz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 50000
+
+# include "libft/libft.h"
+# include <libc.h>
+
+typedef struct		s_file
+{
+	char			*line;
+	int				fd;
+}					t_file;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
